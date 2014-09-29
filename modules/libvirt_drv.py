17ea8eda1164713a6ddc24c9ce60fbb114ad50c6
vizvekov <vizvekov@mirantis.com>
2014-09-29 13:39:27 +0400

    fix

--- modules/libvirt_drv.py
+++ modules/libvirt_drv.py
@@ -11,30 +11,26 @@ class Connect():
         self.__login = login
         self.__password = password
         self.__lv = libvirt
-        conn = self.__connect()
-        main_xml = conn.getCapabilities()
+        self.__connect()
+        main_xml = self.__conn.getCapabilities()
         self.__root = ET.fromstring(main_xml)
         self.uuid = self.__root.find('./host/').text
 
-
     def __connect(self):
-        return self.__lv.open("qemu+ssh://%s@%s/system" % (self.__login, self.__host))
+        self.__conn = self.__lv.open("qemu+ssh://%s@%s/system" % (self.__login, self.__host))
 
     def get_instance_desc(self, uuid, desc_hash=False):
-        conn = self.__connect()
-        return conn.lookupByUUIDString(uuid).XMLDesc()
+        return self.__conn.lookupByUUIDString(uuid).XMLDesc()
 
     def get_instancs(self, run_only=True):
         instance = []
-        conn = self.__connect()
-        for instance_id in conn.listDomainsID():
-            dom = conn.lookupByID(instance_id)
+        for instance_id in self.__conn.listDomainsID():
+            dom = self.__conn.lookupByID(instance_id)
             if dom.isActive() or run_only:
                 instance.append({'uuid': dom.UUIDString(),
                                  'desc': dom.XMLDesc(),
                                  'status': dom.isActive(),
                                  'desc_hash': get_hash(dom.XMLDesc())})
-        conn.close()
         return instance
 
     def __del__(self):
