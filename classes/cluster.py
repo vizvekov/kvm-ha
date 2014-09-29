da47edecce4207a6408ecc1462940f1acc5421ca
vizvekov <vizvekov@mirantis.com>
2014-09-29 13:29:53 +0400

    Revert "Revert "add project""
    This reverts commit 05d06e899bf7458161790fcb286efb1f075395fc.

---
+++ classes/cluster.py
@@ -0,0 +1,55 @@
+__author__ = 'Vadim'
+
+from mod_loader import import_module, get_index, update_dic_in_list, get_hash
+import hashlib, threading
+
+
+class Cluster():
+
+    def __init__(self, driver='libvirt_drv', mod_path='modules'):
+        self.__driver = driver
+        self.__mod_path = mod_path
+        self.__nodes = {}
+        self.__instances = []
+
+    def add_node(self, auth={}):
+        try:
+            uuid, load_driver = import_module(self.__mod_path,
+                                        self.__driver,
+                                       'Connect').Connect(host=auth['host'],
+                                                               login=auth['login'],
+                                                               password=auth['password'])
+        except Exception, error:
+            print(error)
+            return False
+        self.__nodes.update({uuid: load_driver})
+        return True
+
+    def live_migrate_instance(self, instance_id):
+        pass
+
+    def __update_nodes_status(self):
+        for node_uuid, driver in self.__nodes.iteritems():
+            raw_list = driver.get_instancs()
+            for instance in raw_list:
+                try:
+                    old_instance = get_index(self.__instances, 'uuid', instance['uuid'])
+                    disc = driver.get_instance_desc(instance['uuid'])
+                    if get_hash(disc) != old_instance['desc_hash']:
+                        old_instance['desc_hash'] = get_hash(disc)
+                        old_instance['desc'] = disc
+                except Exception, error:
+                    print error
+                    self.__instances.append({'uuid': instance['uuid'],
+                                         'status': instance['status'],
+                                         'on_node': node_uuid,
+                                         'desc_hash': instance['desc_hash'],
+                                         'desc': instance['desc']})
+            self.__timer = threading.Timer(60, self.__update_nodes_status())
+            self.__timer.start()
+
+    def __restart_on_other_node(self, node_id, instance_id):
+        pass
+
+    def __del__(self):
+        pass
---
+++ modules/libvirt_drv.py
@@ -0,0 +1,41 @@
+__author__ = 'Vadim'
+
+import libvirt
+from mod_loader import get_hash
+from xml.etree import ElementTree as ET
+
+class Connect():
+
+    def __init__(self, host='', login='', password=''):
+        self.__host = host
+        self.__login = login
+        self.__password = password
+        self.__lv = libvirt
+        conn = self.__connect()
+        main_xml = conn.getCapabilities()
+        self.__root = ET.fromstring(main_xml)
+        uuid = self.__root.find('./host/').text()
+        return uuid, self
+
+    def __connect(self):
+        return self.__lv.open("qemu+ssh://%s@%s/system" % (self.__login, self.__host))
+
+    def get_instance_desc(self, uuid, desc_hash=False):
+        conn = self.__connect()
+        return conn.lookupByUUIDString(uuid).XMLDesc()
+
+    def get_instancs(self, run_only=True):
+        instance = []
+        conn = self.__connect()
+        for instance_id in conn.listDomainsID():
+            dom = conn.lookupByID(instance_id)
+            if dom.isActive() or run_only:
+                instance.append({'uuid': dom.UUIDString(),
+                                 'desc': dom.XMLDesc(),
+                                 'status': dom.isActive(),
+                                 'desc_hash': get_hash(dom.XMLDesc())})
+        conn.close()
+        return instance
+
+    def __del__(self):
+        pass
